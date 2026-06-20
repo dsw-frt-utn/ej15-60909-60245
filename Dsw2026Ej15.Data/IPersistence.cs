cbf8336f15e2088d7d0c08b9f6639f1812f2ce3f
Juan Pablo <juanpytorresdiaz@gmail.com>
2026-06-20 02:17:47 -0300

    Agregada logica LINQ en PersistenceInMemory

---
+++ Dsw2026Ej15.Data/IPersistence.cs
@@ -0,0 +1,18 @@
+﻿using Dsw2026Ej15.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace Dsw2026Ej15.Data
+{
+    public interface IPersistence
+    {
+        void LoadSpecialities();
+
+        void AddDoctor(Doctor doctor);
+        IEnumerable<Doctor> GetActiveDoctors();
+        Doctor GetDoctorById(Guid id);
+        void DeleteDoctor(Guid id);
+
+        Speciality GetSpecialityByName(string name);
+    }
+}
---
+++ Dsw2026Ej15.Data/PersistenceInMemory.cs
@@ -0,0 +1,50 @@
+﻿using Dsw2026Ej15.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dsw2026Ej15.Data
+{
+    public class PersistenceInMemory : IPersistence
+    {
+        private readonly List<Doctor> _doctors;
+        private readonly List<Speciality> _specialities;
+
+        private static PersistenceInMemory _instance;
+        private static readonly object _lock = new object();
+
+        private PersistenceInMemory()
+        {
+            _doctors = new List<Doctor>();
+            _specialities = new List<Speciality>();
+
+            LoadSpecialities();
+        }
+
+        public static PersistenceInMemory Instance
+        {
+            get
+            {
+                lock (_lock) 
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new PersistenceInMemory();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        public void LoadSpecialities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddDoctor(Doctor doctor) { throw new NotImplementedException(); }
+        public IEnumerable<Doctor> GetActiveDoctors() { throw new NotImplementedException(); }
+        public Doctor GetDoctorById(Guid id) { throw new NotImplementedException(); }
+        public void DeleteDoctor(Guid id) { throw new NotImplementedException(); }
+        public Speciality GetSpecialityByName(string name) { throw new NotImplementedException(); }
+    }
+}
