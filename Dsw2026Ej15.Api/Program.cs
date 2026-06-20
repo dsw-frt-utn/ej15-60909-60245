ad7496f85535f0c90b696ff0af6cdb7052f10208
77Man <SamirFarhat@gmail.com>
2026-06-20 02:45:19 -0300

    MIddleware OK, Persistencia OK y Program.cs unificado

--- Dsw2026Ej15.Api/Program.cs
+++ Dsw2026Ej15.Api/Program.cs
@@ -1,23 +1,28 @@
+using Dsw2026Ej15.Api.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddHealthChecks();
+
+builder.Services.AddSingleton<Dsw2026Ej15.Data.IPersistence>(Dsw2026Ej15.Data.PersistenceInMemory.Instance);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHealthChecks("/health-check");
 
 app.Run();
